ea057d643e6007aaea57df0b0f813a2065f28716
ruiasribeiro <ruiandreribeiro@proton.me>
2023-07-10 13:26:02 +0100

    Add single implementation for splittable

---
+++ include/splittable/single/single.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "splittable/splittable.hpp"
+#include "wstm/stm.h"
+
+namespace splittable::single {
+class single : public splittable {
+ private:
+  WSTM::WVar<uint> value;
+
+ public:
+   // TODO: this is not private because of make_shared, need to revise that later
+  single(uint value);
+
+  auto static new_instance(uint value) -> std::shared_ptr<single>;
+  auto static delete_instance(std::shared_ptr<single>) -> void;
+
+  auto read(WSTM::WAtomic& at) -> uint;
+  // auto inconsistent_read(WSTM::WInconsistent& inc) -> uint;
+  auto add(WSTM::WAtomic& at, uint value) -> void;
+  auto sub(WSTM::WAtomic& at, uint value) -> void;
+};
+}  // namespace splittable::single
---
+++ source/single/single.cpp
@@ -0,0 +1,31 @@
+#include "splittable/single/single.hpp"
+
+namespace splittable::single {
+single::single(uint value) : value(value) {}
+
+auto single::new_instance(uint value) -> std::shared_ptr<single> {
+  return std::make_shared<single>(value);
+}
+
+auto single::delete_instance(std::shared_ptr<single>) -> void {}
+
+auto single::read(WSTM::WAtomic& at) -> uint { return this->value.Get(at); }
+
+auto single::add(WSTM::WAtomic& at, uint value) -> void {
+  auto current = this->value.Get(at);
+  current += value;
+  this->value.Set(current, at);
+}
+
+auto single::sub(WSTM::WAtomic& at, uint value) -> void {
+  auto current = this->value.Get(at);
+
+  if (current < value) {
+    // TODO: change this to a better exception
+    throw std::exception();
+  }
+
+  current -= value;
+  this->value.Set(current, at);
+}
+}  // namespace splittable::single
