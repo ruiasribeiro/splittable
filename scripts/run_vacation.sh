3754492c0bf3e2ca1d27fe411fcd74ce9e62f12d
ruiasribeiro <ruiandreribeiro@proton.me>
2023-07-11 10:09:56 +0100

    Add Vacation test script

---
+++ scripts/run_vacation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+splittable_list=(SINGLE MRV_FLEX_VECTOR PR_ARRAY)
+client_list=(1 2 4 8 16 32 64 128)
+runs=1
+
+vacation_params=(-n4 -q60 -u90 -r1048576 -T4194304)
+
+for type in ${splittable_list[@]}; do
+    make clean
+    SPLITTABLE_TYPE="-DSPLITTABLE_USE_${type}" make 
+    
+    for client in ${client_list[@]}; do
+        for _ in $(seq $runs); do
+            ./build/bin/vacation ${vacation_params[@]} -t${client}
+        done
+    done
+done
