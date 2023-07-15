3a8880c74955137c4419037b27a4ce35102b73c2
ruiasribeiro <ruiandreribeiro@proton.me>
2023-07-15 15:00:23 +0100

    Improve graph generator

--- scripts/generate_client_graph.py
+++ scripts/generate_client_graph.py
@@ -13,8 +13,6 @@ args = parser.parse_args()
 
 df = pd.read_csv(args.csv_path)
 
-# df["workers"] = df["workers"].astype(str)
-
 df = (
     df.groupby(["benchmark", "workers"])
     .agg(
@@ -27,12 +25,8 @@ df = (
     .reset_index()
 )
 
-
-# print(df)
-
-# single_df = df[df['benchmark'] == 'single']
-# mrv_flex_vector_df = df[df['benchmark'] == 'mrv-flex-vector']
-# pr_array_df = df[df['benchmark'] == 'pr-array']
+marker = ["o", "v", "^", "<", ">", "8", "s", "p", "*", "h", "H", "D", "d", "P", "X"]
+markers = [marker[i] for i in range(len(df["benchmark"].unique()))]
 
 # sns.set_theme()
 plt.xscale("log")
@@ -40,32 +34,30 @@ plt.xscale("log")
 ticks = df[df["benchmark"] == "single"]["workers"]
 plt.xticks(ticks, ticks)
 
-sns.set_style("white", {"axes.linewidth": 0.5})
-sns.lineplot(
-    data=df,
-    x="workers",
-    y="abort rate",
-    hue="benchmark",
-    marker="o",
-    linestyle='--',
-    legend=None,
-    palette="pastel"
-    # order=["1", "2", "4", "8", "16", "32", "64", "128"],
-    # sort=False,
-)
+ax1 = plt.subplot()
 ax2 = plt.twinx()
+
 sns.lineplot(
     ax=ax2,
     data=df,
     x="workers",
+    y="abort rate",
+    hue="benchmark",
+    style="benchmark",
+    markers=markers,
+    legend=None,
+    alpha=0.35,
+)
+plt.ylim([0, 1])
+
+sns.lineplot(
+    ax=ax1,
+    data=df,
+    x="workers",
     y="throughput (ops/s)",
     hue="benchmark",
-    marker="o",
-    palette="bright"
-    # order=["1", "2", "4", "8", "16", "32", "64", "128"],
-    # sort=False,
+    style="benchmark",
+    markers=markers,
 )
-# sns.lineplot(data=mrv_flex_vector_df, kind="line", x="workers", y="throughput (ops/s)")
-# sns.lineplot(data=pr_array_df, kind="line", x="workers", y="throughput (ops/s)")
 
-plt.savefig("test.png")
+plt.savefig("test.pdf")
--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -2,9 +2,10 @@
 
 worker_list=(1 2 4 8 16 32 64 128)
 type_list=(single mrv-flex-vector pr-array)
-padding_list=(10 1000 100000 10000000 1000000000)
-seconds=5
-runs=1
+# padding_list=(10 1000 100000 10000000)
+padding_list=(100000)
+seconds=60
+runs=5
 
 printf "benchmark,workers,execution time (s),padding,commited operations,throughput (ops/s),abort rate\n"
 
