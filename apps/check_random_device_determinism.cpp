9dbff7822caf8ebe9761f3599418f70d8b58841e
ruiasribeiro <ruiandreribeiro@proton.me>
2023-07-20 15:27:49 +0100

    Add `std::random_device` checker

---
+++ apps/check_random_device_determinism.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <mutex>
+#include <random>
+#include <thread>
+#include <vector>
+
+int main(int argc, char const *argv[]) {
+  auto num_threads = 10ul;
+
+  std::vector<std::thread> threads;
+  threads.reserve(num_threads);
+
+  std::mutex cout_mutex;
+
+  for (auto i = 0ul; i < num_threads; ++i) {
+    threads.emplace_back([i, &cout_mutex]() {
+      auto num = std::random_device{}();
+
+      {
+        std::lock_guard<std::mutex> guard(cout_mutex);
+        std::cout << "thread " << i << ": " << num << "\n";
+      }
+    });
+  }
+
+  for (auto i = 0ul; i < num_threads; ++i) {
+    threads[i].join();
+  }
+
+  return 0;
+}
