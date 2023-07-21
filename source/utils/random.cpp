924a3f7584ddbc1a9f222d50ff798d017de45b98
Rui Ribeiro <46627071+ruiasribeiro@users.noreply.github.com>
2023-07-21 18:41:55 +0100

    Fix RNG
    Adding `thread_local` to the distribution was a mistake; `min` and `max` are not always the same.

--- source/utils/random.cpp
+++ source/utils/random.cpp
@@ -8,19 +8,19 @@ auto random_index(size_t min, size_t max) -> size_t {
 
 auto random_index_mt19937(size_t min, size_t max) -> size_t {
   thread_local std::mt19937 generator(std::random_device{}());
-  thread_local std::uniform_int_distribution<size_t> distribution(min, max);
+  std::uniform_int_distribution<size_t> distribution(min, max);
   return distribution(generator);
 }
 
 auto random_index_minstd_rand(size_t min, size_t max) -> size_t {
   thread_local std::minstd_rand generator(std::random_device{}());
-  thread_local std::uniform_int_distribution<size_t> distribution(min, max);
+  std::uniform_int_distribution<size_t> distribution(min, max);
   return distribution(generator);
 }
 
 auto random_index_ranlux24_base(size_t min, size_t max) -> size_t {
   thread_local std::ranlux24_base generator(std::random_device{}());
-  thread_local std::uniform_int_distribution<size_t> distribution(min, max);
+  std::uniform_int_distribution<size_t> distribution(min, max);
   return distribution(generator);
 }
 
