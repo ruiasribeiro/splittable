b5d399741d94fe79b584c3f940c1ca87ba60437a
ruiasribeiro <ruiandreribeiro@proton.me>
2023-07-24 22:57:42 +0100

    Add command line parser to microbenchmark

--- apps/microbenchmark.cpp
+++ apps/microbenchmark.cpp
@@ -1,6 +1,7 @@
 #include <wstm/stm.h>
 
 #include <atomic>
+#include <boost/program_options.hpp>
 #include <boost/thread/barrier.hpp>
 #include <chrono>
 #include <cstdint>
@@ -24,6 +25,15 @@ struct result_t {
   double abort_rate;
 };
 
+struct options_t {
+  std::string benchmark;
+  size_t num_workers;
+  size_t read_percentage;
+  seconds duration;
+  size_t time_padding;
+  size_t num_threads_pool;
+};
+
 double waste_time(size_t iterations) {
   auto value = 1;
 
@@ -35,20 +45,19 @@ double waste_time(size_t iterations) {
 }
 
 template <typename S>
-result_t run(size_t workers, size_t read_percentage, seconds duration,
-             size_t time_padding, size_t num_threads_pool) {
+result_t run(options_t options) {
   auto total_reads = std::atomic_uint(0);
   auto total_writes = std::atomic_uint(0);
 
   auto value = S::new_instance(0);
-  S::global_init(workers, num_threads_pool);
+  S::global_init(options.num_workers, options.num_threads_pool);
 
-  auto threads = std::make_unique<std::thread[]>(workers);
+  auto threads = std::make_unique<std::thread[]>(options.num_workers);
 
-  boost::barrier bar(workers + 1);
+  boost::barrier bar(options.num_workers + 1);
 
-  for (size_t i = 0; i < workers; i++) {
-    threads[i] = std::thread([&, i, duration]() {
+  for (size_t i = 0; i < options.num_workers; i++) {
+    threads[i] = std::thread([&, i, options]() {
       S::thread_init();
 
       double val{};
@@ -61,26 +70,27 @@ result_t run(size_t workers, size_t read_percentage, seconds duration,
       auto now = steady_clock::now;
       auto start = now();
 
-      while ((now() - start) < duration) {
+      while ((now() - start) < options.duration) {
         auto random_pick = splittable::utils::random_index(1, 100);
 
         try {
           WSTM::Atomically([&](WSTM::WAtomic& at) {
-            val = waste_time(time_padding);
+            val = waste_time(options.time_padding);
 
-            if (random_pick <= read_percentage) {
+            if (random_pick <= options.read_percentage) {
               val2 = value->read(at);
             } else if (static_cast<double>(random_pick) <=
-                       (static_cast<double>(read_percentage) / 2) + 50) {
+                       (static_cast<double>(options.read_percentage) / 2) +
+                           50) {
               value->sub(at, 1);
             } else {
               value->add(at, 1);
             }
 
-            val = waste_time(time_padding);
+            val = waste_time(options.time_padding);
           });
 
-          if (random_pick <= read_percentage) {
+          if (random_pick <= options.read_percentage) {
             reads++;
           } else {
             writes++;
@@ -101,7 +111,7 @@ result_t run(size_t workers, size_t read_percentage, seconds duration,
 
   bar.wait();
 
-  for (size_t i = 0; i < workers; i++) {
+  for (size_t i = 0; i < options.num_workers; i++) {
     threads[i].join();
   }
 
@@ -115,97 +125,57 @@ result_t run(size_t workers, size_t read_percentage, seconds duration,
 }
 
 int main(int argc, char const* argv[]) {
-  if (argc < 7) {
-    std::cerr << "requires 6 positional arguments: benchmark, number of "
-                 "workers, read percentage (%), execution time (s), padding, "
-                 "number of pool workers\n";
-    return 1;
-  }
+  namespace po = boost::program_options;
 
-  int workers;
-  try {
-    workers = std::stoi(argv[2]);
-  } catch (...) {
-    std::cerr << "could not convert \"" << argv[2] << "\" to an integer\n";
-    return 1;
-  }
+  options_t options;
+  po::options_description description("Allowed options");
 
-  if (workers < 1) {
-    std::cerr << "minimum of 1 worker is required\n";
-    return 1;
-  }
+  // clang-format off
+  description.add_options()
+    ("help,h", "produce help message")
+    ("benchmark,b", po::value<std::string>(), "set splittable type")
+    ("num_workers,w", po::value<size_t>(), "set number of clients for the benchmark")
+    ("read_percentage,r", po::value<size_t>(), "set percentage of read operations")
+    ("duration,d", po::value<size_t>(), "set benchmark duration (in seconds)")
+    ("time_padding,p", po::value<size_t>(), "set padding for the transactions")
+    ("num_threads_pool,t", po::value<size_t>(), "set number of threads for the splittable pool");
+  // clang-format on
 
-  size_t read_percentage;
-  try {
-    read_percentage = std::stoul(argv[3]);
-  } catch (...) {
-    std::cerr << "could not convert \"" << argv[3] << "\" to an integer\n";
-    return 1;
-  }
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, description), vm);
+  po::notify(vm);
 
-  if (read_percentage > 100) {
-    std::cerr << "read percentage should be between 0 and 100, got "
-              << read_percentage << "\n";
-    return 1;
-  }
-
-  seconds duration;
-  try {
-    duration = seconds{std::stoi(argv[4])};
-  } catch (...) {
-    std::cerr << "could not convert \"" << argv[4] << "\" to an integer\n";
-    return 1;
-  }
-
-  if (duration.count() < 1) {
-    std::cerr << "minimum of 1 second is required\n";
-    return 1;
-  }
-
-  size_t padding;
-  try {
-    padding = std::stoul(argv[5]);
-  } catch (...) {
-    std::cerr << "could not convert \"" << argv[5] << "\" to an integer\n";
-    return 1;
-  }
-
-  size_t pool_workers;
-  try {
-    pool_workers = std::stoul(argv[6]);
-  } catch (...) {
-    std::cerr << "could not convert \"" << argv[6] << "\" to an integer\n";
-    return 1;
-  }
-
-  std::string benchmark(argv[1]);
+  options.benchmark = vm["benchmark"].as<std::string>();
+  options.num_workers = vm["num_workers"].as<size_t>();
+  options.read_percentage = vm["read_percentage"].as<size_t>();
+  options.duration = seconds{vm["duration"].as<size_t>()};
+  options.time_padding = vm["time_padding"].as<size_t>();
+  options.num_threads_pool = vm["num_threads_pool"].as<size_t>();
 
   result_t result;
-  if (benchmark == "single") {
+  if (options.benchmark == "single") {
     using splittable_t = splittable::single::single;
-    result = run<splittable_t>(workers, read_percentage, duration, padding,
-                               pool_workers);
-  } else if (benchmark == "mrv-flex-vector") {
+    result = run<splittable_t>(options);
+  } else if (options.benchmark == "mrv-flex-vector") {
     using splittable_t = splittable::mrv::mrv_flex_vector;
-    result = run<splittable_t>(workers, read_percentage, duration, padding,
-                               pool_workers);
-  } else if (benchmark == "pr-array") {
+    result = run<splittable_t>(options);
+  } else if (options.benchmark == "pr-array") {
     using splittable_t = splittable::pr::pr_array;
-    result = run<splittable_t>(workers, read_percentage, duration, padding,
-                               pool_workers);
+    result = run<splittable_t>(options);
   } else {
-    std::cerr << "could not find a benchmark with name \"" << benchmark
+    std::cerr << "could not find a benchmark with name \"" << options.benchmark
               << "\"; try\"single\", \"mrv-flex-vector\", \"pr-array\"\n";
     return 1;
   }
 
   // CSV: benchmark, workers, execution time, read percentage, writes, reads,
   // write throughput (ops/s), read throughput (ops/s), abort rate
-  std::cout << benchmark << "," << workers << "," << duration.count() << ","
-            << read_percentage << "," << result.writes << "," << result.reads
-            << "," << result.writes / duration.count() << ","
-            << result.reads / duration.count() << "," << result.abort_rate
-            << "\n";
+  std::cout << options.benchmark << "," << options.num_workers << ","
+            << options.duration.count() << "," << options.read_percentage << ","
+            << result.writes << "," << result.reads << ","
+            << result.writes / options.duration.count() << ","
+            << result.reads / options.duration.count() << ","
+            << result.abort_rate << "\n";
 
   // return 0;
   quick_exit(0);
