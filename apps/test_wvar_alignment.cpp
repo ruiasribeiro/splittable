c8f2f429cbdd9c34e09571214e0acdb208e54b9c
ruiasribeiro <ruiandreribeiro@proton.me>
2023-07-27 13:35:20 +0100

    Add transactional variable alignment test

---
+++ apps/test_wvar_alignment.cpp
@@ -0,0 +1,197 @@
+#include <wstm/stm.h>
+
+#include <atomic>
+#include <boost/program_options.hpp>
+#include <boost/thread/barrier.hpp>
+#include <chrono>
+#include <cstdint>
+#include <immer/flex_vector.hpp>
+#include <immer/flex_vector_transient.hpp>
+#include <iostream>
+#include <mutex>
+#include <optional>
+#include <thread>
+#include <vector>
+
+using std::chrono::seconds;
+using std::chrono::steady_clock;
+
+using namespace std::chrono_literals;
+
+const seconds warmup(5);
+
+struct result_t {
+  uint writes;
+};
+
+struct options_t {
+  std::string benchmark;
+  size_t num_workers;
+  seconds duration;
+};
+
+struct alignas(std::hardware_destructive_interference_size) aligned_wvar_t
+    : public WSTM::WVar<uint> {
+  // inherit all of the constructors
+  using WSTM::WVar<uint>::WVar;
+};
+
+template <typename T>
+result_t run_immer(options_t options) {
+  auto total_writes = std::atomic_uint(0);
+  auto threads = std::make_unique<std::thread[]>(options.num_workers);
+
+  immer::flex_vector<std::shared_ptr<T>> chunks;
+  auto t = chunks.transient();
+  for (auto i = 0u; i < options.num_workers; ++i) {
+    t.push_back(std::make_shared<T>(0u));
+  }
+  chunks = t.persistent();
+
+  boost::barrier bar(options.num_workers + 1);
+
+  for (size_t i = 0; i < options.num_workers; i++) {
+    threads[i] = std::thread([&, i, options]() {
+      size_t writes = 0;
+
+      bar.wait();
+
+      auto now = steady_clock::now;
+      auto start = now();
+
+      while ((now() - start) < warmup) {
+        WSTM::Atomically([&](WSTM::WAtomic& at) {
+          chunks[i]->Set(chunks[i]->Get(at) + 1, at);
+        });
+      }
+
+      start = now();
+
+      while ((now() - start) < options.duration) {
+        WSTM::Atomically([&](WSTM::WAtomic& at) {
+          chunks[i]->Set(chunks[i]->Get(at) + 1, at);
+        });
+
+        writes++;
+      }
+
+      total_writes.fetch_add(writes);
+    });
+  }
+
+  bar.wait();
+
+  for (size_t i = 0; i < options.num_workers; i++) {
+    threads[i].join();
+  }
+
+  return {.writes = total_writes.load()};
+}
+
+template <typename T>
+result_t run_std(options_t options) {
+  auto total_writes = std::atomic_uint(0);
+  auto threads = std::make_unique<std::thread[]>(options.num_workers);
+
+  std::vector<T> chunks;
+  chunks.reserve(options.num_workers);
+  for (auto i = 0u; i < options.num_workers; ++i) {
+    chunks.emplace_back(0u);
+  }
+
+  boost::barrier bar(options.num_workers + 1);
+
+  for (size_t i = 0; i < options.num_workers; i++) {
+    threads[i] = std::thread([&, i, options]() {
+      size_t writes = 0;
+
+      bar.wait();
+
+      auto now = steady_clock::now;
+      auto start = now();
+
+      while ((now() - start) < warmup) {
+        WSTM::Atomically([&](WSTM::WAtomic& at) {
+          chunks[i].Set(chunks[i].Get(at) + 1, at);
+        });
+      }
+
+      start = now();
+
+      while ((now() - start) < options.duration) {
+        WSTM::Atomically([&](WSTM::WAtomic& at) {
+          chunks[i].Set(chunks[i].Get(at) + 1, at);
+        });
+
+        writes++;
+      }
+
+      total_writes.fetch_add(writes);
+    });
+  }
+
+  bar.wait();
+
+  for (size_t i = 0; i < options.num_workers; i++) {
+    threads[i].join();
+  }
+
+  return {.writes = total_writes.load()};
+}
+
+int main(int argc, char const* argv[]) {
+  namespace po = boost::program_options;
+
+  options_t options;
+  po::options_description description("Allowed options");
+
+  // clang-format off
+  description.add_options()
+    ("help,h", "produce help message")
+    ("benchmark,b", 
+      po::value<std::string>()->default_value(""), 
+      "set splittable type")
+    ("num_workers,w", 
+      po::value<size_t>()->required(), 
+      "set number of clients for the benchmark")
+    ("duration,d", 
+      po::value<size_t>()->required(),  
+      "set benchmark duration (in seconds)");
+  // clang-format on
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, description), vm);
+  po::notify(vm);
+
+  options.benchmark = vm["benchmark"].as<std::string>();
+  options.num_workers = vm["num_workers"].as<size_t>();
+  options.duration = seconds{vm["duration"].as<size_t>()};
+
+  result_t result;
+  if (options.benchmark == "immer-wvar") {
+    using wvar_t = WSTM::WVar<uint>;
+    result = run_immer<wvar_t>(options);
+  } else if (options.benchmark == "immer-aligned-wvar") {
+    using wvar_t = aligned_wvar_t;
+    result = run_immer<wvar_t>(options);
+  } else if (options.benchmark == "std-wvar") {
+    using wvar_t = WSTM::WVar<uint>;
+    result = run_std<wvar_t>(options);
+  } else if (options.benchmark == "std-aligned-wvar") {
+    using wvar_t = aligned_wvar_t;
+    result = run_std<wvar_t>(options);
+  } else {
+    std::cerr << "could not find a benchmark with name \"" << options.benchmark
+              << "\"; try \"immer-wvar\", \"immer-aligned-wvar\", "
+                 "\"std-wvar\", \"std-aligned-wvar\"\n";
+    return 1;
+  }
+
+  // CSV: benchmark, workers, execution time, writes, throughput (ops/s)
+  std::cout << options.benchmark << "," << options.num_workers << ","
+            << options.duration.count() << "," << result.writes << ","
+            << result.writes / options.duration.count() << "\n";
+
+  // return 0;
+  quick_exit(0);
+}
---
+++ scripts/run_test_wvar_alignment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+type_list=(immer-wvar immer-aligned-wvar std-wvar std-aligned-wvar)
+worker_list=(8)
+seconds=60
+runs=5
+
+printf "benchmark,workers,execution time (s),writes,throughput (ops/s)\n"
+
+for type in ${type_list[@]}; do
+    for workers in ${worker_list[@]}; do
+        for _ in $(seq $runs); do
+            ./build/bin/test_wvar_alignment -b ${type} -w ${workers} -d ${seconds}
+        done
+    done
+done
