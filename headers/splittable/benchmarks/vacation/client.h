108dfcb48e8b0f27c830b3a8b6ddc045caa4c975
ruiasribeiro <ruiandreribeiro@proton.me>
2023-08-04 14:01:35 +0100

    Simplify Vacation restarts

--- headers/splittable/benchmarks/vacation/client.h
+++ headers/splittable/benchmarks/vacation/client.h
@@ -91,93 +91,80 @@ void client_run(void* argPtr) {
           ids[n] = (randomPtr() % queryRange) + 1;
         }
         bool isFound = false;
-        bool done = false;
-        //[wer210] I modified here to remove _ITM_abortTransaction().
-        while (!done) {
-          try {
-            WSTM::Atomically([&](WSTM::WAtomic& at) {
-              done = true;
 
-              for (n = 0; n < numQuery; n++) {
-                long t = types[n];
-                long id = ids[n];
-                long price = -1;
-                switch (t) {
-                  case RESERVATION_CAR:
-                    if (managerPtr->queryCar(at, id) >= 0) {
-                      price = managerPtr->queryCarPrice(at, id);
-                    }
-                    break;
-                  case RESERVATION_FLIGHT:
-                    if (managerPtr->queryFlight(at, id) >= 0) {
-                      price = managerPtr->queryFlightPrice(at, id);
-                    }
-                    break;
-                  case RESERVATION_ROOM:
-                    if (managerPtr->queryRoom(at, id) >= 0) {
-                      price = managerPtr->queryRoomPrice(at, id);
-                    }
-                    break;
-                  default:
-                    assert(0);
+        WSTM::Atomically([&](WSTM::WAtomic& at) {
+          bool done = true;
+
+          for (n = 0; n < numQuery; n++) {
+            long t = types[n];
+            long id = ids[n];
+            long price = -1;
+            switch (t) {
+              case RESERVATION_CAR:
+                if (managerPtr->queryCar(at, id) >= 0) {
+                  price = managerPtr->queryCarPrice(at, id);
                 }
-                //[wer210] read-only above
-                if (price > maxPrices[t]) {
-                  maxPrices[t] = price;
-                  maxIds[t] = id;
-                  isFound = true;
+                break;
+              case RESERVATION_FLIGHT:
+                if (managerPtr->queryFlight(at, id) >= 0) {
+                  price = managerPtr->queryFlightPrice(at, id);
                 }
-              } /* for n */
+                break;
+              case RESERVATION_ROOM:
+                if (managerPtr->queryRoom(at, id) >= 0) {
+                  price = managerPtr->queryRoomPrice(at, id);
+                }
+                break;
+              default:
+                assert(0);
+            }
+            //[wer210] read-only above
+            if (price > maxPrices[t]) {
+              maxPrices[t] = price;
+              maxIds[t] = id;
+              isFound = true;
+            }
+          } /* for n */
 
-              if (isFound) {
-                done = done && managerPtr->addCustomer(at, customerId);
-              }
-
-              if (maxIds[RESERVATION_CAR] > 0) {
-                done = done && managerPtr->reserveCar(at, customerId,
-                                                      maxIds[RESERVATION_CAR]);
-              }
-              if (maxIds[RESERVATION_FLIGHT] > 0) {
-                done = done && managerPtr->reserveFlight(
-                                   at, customerId, maxIds[RESERVATION_FLIGHT]);
-              }
-              if (maxIds[RESERVATION_ROOM] > 0) {
-                done = done && managerPtr->reserveRoom(
-                                   at, customerId, maxIds[RESERVATION_ROOM]);
-              }
-
-              if (!done) {
-                // assert(0);
-                throw cancel_transaction();
-              }
-            });
-          } catch (cancel_transaction const&) {
-            done = false;
+          if (isFound) {
+            done = done && managerPtr->addCustomer(at, customerId);
           }
-        }
+
+          if (maxIds[RESERVATION_CAR] > 0) {
+            done = done && managerPtr->reserveCar(at, customerId,
+                                                  maxIds[RESERVATION_CAR]);
+          }
+          if (maxIds[RESERVATION_FLIGHT] > 0) {
+            done = done && managerPtr->reserveFlight(
+                               at, customerId, maxIds[RESERVATION_FLIGHT]);
+          }
+          if (maxIds[RESERVATION_ROOM] > 0) {
+            done = done && managerPtr->reserveRoom(at, customerId,
+                                                   maxIds[RESERVATION_ROOM]);
+          }
+
+          if (!done) {
+            WSTM::Retry(at);
+          }
+        });
+
         break;
       }
 
       case ACTION_DELETE_CUSTOMER: {
         long customerId = randomPtr() % queryRange + 1;
-        bool done = false;
-        while (!done) {
-          try {
-            WSTM::Atomically([&](WSTM::WAtomic& at) {
-              done = true;
-              long bill = managerPtr->queryCustomerBill(at, customerId);
-              if (bill >= 0) {
-                done = done && managerPtr->deleteCustomer(at, customerId);
-              }
-              if (!done) {
-                // assert(0);
-                throw cancel_transaction();
-              }
-            });
-          } catch (cancel_transaction const&) {
-            done = false;
+
+        WSTM::Atomically([&](WSTM::WAtomic& at) {
+          bool done = true;
+          long bill = managerPtr->queryCustomerBill(at, customerId);
+          if (bill >= 0) {
+            done = done && managerPtr->deleteCustomer(at, customerId);
           }
-        }
+          if (!done) {
+            WSTM::Retry(at);
+          }
+        });
+
         break;
       }
 
@@ -192,56 +179,48 @@ void client_run(void* argPtr) {
             prices[n] = ((randomPtr() % 5) * 10) + 50;
           }
         }
-        bool done = false;
-        while (!done) {
-          try {
-            WSTM::Atomically([&](WSTM::WAtomic& at) {
-              done = true;
-              for (n = 0; n < numUpdate; n++) {
-                long t = types[n];
-                long id = ids[n];
-                long doAdd = ops[n];
-                if (doAdd) {
-                  long newPrice = prices[n];
-                  switch (t) {
-                    case RESERVATION_CAR:
-                      done = done && managerPtr->addCar(at, id, 100, newPrice);
-                      break;
-                    case RESERVATION_FLIGHT:
-                      done =
-                          done && managerPtr->addFlight(at, id, 100, newPrice);
-                      break;
-                    case RESERVATION_ROOM:
-                      done = done && managerPtr->addRoom(at, id, 100, newPrice);
-                      break;
-                    default:
-                      assert(0);
-                  }
-                } else { /* do delete */
-                  switch (t) {
-                    case RESERVATION_CAR:
-                      done = done && managerPtr->deleteCar(at, id, 100);
-                      break;
-                    case RESERVATION_FLIGHT:
-                      done = done && managerPtr->deleteFlight(at, id);
-                      break;
-                    case RESERVATION_ROOM:
-                      done = done && managerPtr->deleteRoom(at, id, 100);
-                      break;
-                    default:
-                      assert(0);
-                  }
-                }
+
+        WSTM::Atomically([&](WSTM::WAtomic& at) {
+          bool done = true;
+          for (n = 0; n < numUpdate; n++) {
+            long t = types[n];
+            long id = ids[n];
+            long doAdd = ops[n];
+            if (doAdd) {
+              long newPrice = prices[n];
+              switch (t) {
+                case RESERVATION_CAR:
+                  done = done && managerPtr->addCar(at, id, 100, newPrice);
+                  break;
+                case RESERVATION_FLIGHT:
+                  done = done && managerPtr->addFlight(at, id, 100, newPrice);
+                  break;
+                case RESERVATION_ROOM:
+                  done = done && managerPtr->addRoom(at, id, 100, newPrice);
+                  break;
+                default:
+                  assert(0);
               }
-              if (!done) {
-                // assert(0);
-                throw cancel_transaction();
+            } else { /* do delete */
+              switch (t) {
+                case RESERVATION_CAR:
+                  done = done && managerPtr->deleteCar(at, id, 100);
+                  break;
+                case RESERVATION_FLIGHT:
+                  done = done && managerPtr->deleteFlight(at, id);
+                  break;
+                case RESERVATION_ROOM:
+                  done = done && managerPtr->deleteRoom(at, id, 100);
+                  break;
+                default:
+                  assert(0);
               }
-            });
-          } catch (cancel_transaction const&) {
-            done = false;
+            }
           }
-        }
+          if (!done) {
+            WSTM::Retry(at);
+          }
+        });
         break;
       }
 
